6925f660edc8c60885c4d8f9a13dcb29a4d0572f
Notedop <raoul2405@hotmail.com>
2019-06-04 21:25:28 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,126 @@
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <max6675.h>
+
+// If using software SPI (the default case):
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+
+#define thermoDO 4
+#define thermoCS 5
+#define thermoCLK 6
+
+#define HEAT_PIN 14
+#define FAN_PIN 15
+
+MAX6675 temp_sensor;
+Adafruit_SSD1306 display(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+const uint8_t numberOfReads = 16;
+const uint8_t screenWidth = 128;
+const uint8_t xWidth = screenWidth/numberOfReads;
+
+const char* OFF = "L";
+const char* ON = "H";
+
+char Celsius_C[8];
+double Celcius_d;
+
+uint8_t temperatureHistory[numberOfReads] = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+uint8_t chartPos = 0;
+
+uint16_t intervalSensorRead = 10000; 
+uint32_t tempMillis = 0;
+
+void setup() {
+  
+  pinMode(FAN_PIN, OUTPUT);
+  digitalWrite(FAN_PIN, HIGH);
+  pinMode(HEAT_PIN, OUTPUT);  
+  digitalWrite(HEAT_PIN, LOW);
+  
+  Serial.begin(9600);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin();
+  // init done
+  //display configurations
+  display.setFont();
+  display.setTextColor(WHITE);
+  display.setTextWrap(false);
+  
+  temp_sensor.begin(thermoCLK, thermoCS, thermoDO);
+
+}
+
+void setTopText(const char* value, uint8_t x, uint8_t y, uint8_t size ) {
+  display.setCursor(x,y);
+  display.setTextSize(size);
+  display.println(value);
+}
+
+void drawChart() {
+  
+  for (int8_t i=0; i<(numberOfReads-1); i++) {
+    display.writeLine(
+      i*xWidth,     map(temperatureHistory[i],  0,255,64,16), 
+      (i+1)*xWidth, map(temperatureHistory[i+1],0,255,64,16),
+      WHITE
+    );
+  }
+}
+
+void addNewValue(uint8_t value) {
+
+  for (int8_t i=0; i<(numberOfReads-1); i++) {
+    temperatureHistory[i] = temperatureHistory[i+1];
+  }
+
+  temperatureHistory[numberOfReads-1] = value;
+
+}
+
+void loop() {
+
+  Celcius_d = temp_sensor.readCelsius();
+  dtostrf(Celcius_d, 4,2, Celsius_C);
+  Serial.println(Celsius_C);
+  display.clearDisplay();
+  
+  while (Serial.available() > 0)
+  {
+    Serial.read();
+  }
+
+
+  if (Celcius_d > 235.0)
+  {
+    digitalWrite(HEAT_PIN, LOW);
+    setTopText(OFF, 112, 0, 2);
+  }
+  else if (Celcius_d == 235.0)
+  {
+    digitalWrite(HEAT_PIN, LOW);  
+    setTopText("-", 112, 0, 2);
+  }
+  else if (Celcius_d < 235.0)
+  {
+    digitalWrite(HEAT_PIN, HIGH);
+    setTopText(ON, 112, 0, 2);
+  }
+
+  setTopText(Celsius_C,0,0,2);
+  drawChart();
+  display.display();
+  display.clearDisplay();
+
+  if ((millis()-tempMillis) > intervalSensorRead) {
+    addNewValue((uint8_t) Celcius_d);
+    tempMillis=millis();
+  }
+  //chartPos=rand();
+  delay(1000);
+}
